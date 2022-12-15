5f9e057f7ae8243f698df5f7d5330dad23feda19
DogeonKim <73727010+Dogeon-Kim@users.noreply.github.com>
2022-12-15 11:02:19 +0900

    Update 단순 연결리스트 삽입&탐색.c

--- Algorithm & Data Structure/Kwak/연결리스트1/단순 연결리스트 삽입&탐색.c
+++ Algorithm & Data Structure/Kwak/연결리스트1/단순 연결리스트 삽입&탐색.c
@@ -84,28 +84,22 @@ Node* searchNode(Node *head, char *x){
 }
 
 int main(){
-	Node *head;
-	Node *p;
-	printf("(1) 공백리스트 생성하기! \n");
-	head=createList();
-	printList(head);
+	Node *head, *p;
+	printf("(1) 공백리스트 생성하기! \n"); 
+	head=createList(); printList(head);
 	printf("(2) 리스트 처음에 [수]노드 삽입하기! \n");
-	head=insertFirst(head, "수");
-	printList(head);
+	head=insertFirst(head, "수"); printList(head);
 	printf("(3) 리스트 마지막에 [금]노드 삽입하기! \n");
-	head=insertLast(head, "금");
-	printList(head);
+	head=insertLast(head, "금"); printList(head);
 	printf("(4) 리스트 첫 번째에 [월]노드 삽입하기! \n");
-	head=insertFirst(head, "월");
-	printList(head);
-	printf("(5) 리스트에서 [수] 노드를 찾아 그 뒤에 [목] 삽입하기! \n");
+	head=insertFirst(head, "월"); printList(head);
+	printf("(5) 리스트에서 [수] 노드를 찾아 그 뒤에 [목] 삽입하기! \n"); 
 	p=searchNode(head, "수");
 	if(p==NULL) printf("찾는 데이터가 없습니다.\n");
 	else head = insertMiddle(head, p, "목");
 	printList(head);
 	printf("(6) 리스트 공간을 해제하여 공백 리스트로 만들기! \n");
-	deleteAll(head);
-	printList(head);
+	deleteAll(head);printList(head);
 	return 0;
 }
 
